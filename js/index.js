471362e30fec8fb9b298e6404e0397838325cd2c
gxl <535301328@qq.com>
2017-10-31 23:16:50 +0800

    跟新了

--- js/index.js
+++ js/index.js
@@ -1,7 +1,21 @@
 //head
-$(".head-li").mouseover(function(){
+$(".head-li").hover(function(){
 	$(this).find("ul").css("display","block");
+},function(){
+	$(this).find("ul").css("display","none");
 })
-$(".head-li ul").mouseout(function(){
-	$(this).css("display","none");
-})
+//banner
+var index=0;
+function banner(){
+	$(".banner ul li").eq(index).animate({"opacity":1},3000)
+					.siblings()
+					.animate({"opacity":0},3000);
+	index++;
+	if(index==$(".banner ul li").length){
+		index=0;
+	}
+}
+banner();
+setInterval(function(){
+	banner();
+},3000)
